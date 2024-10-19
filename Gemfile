368a216b4ae9ec07f556b9ae8f167f26f9eec513
Valentin Cadel <104843541+valencadel@users.noreply.github.com>
2024-10-19 12:43:11 -0300

    simple airbnb a medias

--- Gemfile
+++ Gemfile
@@ -66,3 +66,5 @@ group :test do
 end
 gem 'rspec-rails', group: [ :test ]
 gem 'rails-controller-testing', group: [ :test ]
+
+gem "simple_form"
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,31 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Departamento en Florida',
+  address: 'Francisco N de Laprida 1970',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 200,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'La casa de Pinocho',
+  address: 'Todos cuentan hasta 8',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 55,
+  number_of_guests: 5
+)
+Flat.create!(
+  name: 'Castillo de Melo',
+  address: 'Avenida siempreviva 1234',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 750,
+  number_of_guests: 10
+)
